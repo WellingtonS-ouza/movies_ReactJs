8897a1f85b8a4225b75b4b1afa5151d8b61cc622
Wellington <wel.souza88@gmail.com>
2023-06-29 12:56:13 -0300

    Style the Carousel

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,7 +6,31 @@ import { Paper } from '@mui/material'
 
 const Hero = ({movies}) => {
   return (
-    <div>Hero</div>
+    <div className='movie-carousel-container'>
+        <Carousel>
+        {
+            movies?.map((movie) =>{
+                return(
+                    <Paper>
+                        <div className = 'movie-card-container'>
+                            <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                <div className="movie-detail">
+                                    <div className="movie-poster">
+                                        <img src={movie.poster} alt="" />
+                                    </div>
+                                    <div className="movie-title">
+                                        <h4>{movie.title}</h4>
+                                    </div>
+                               
+                                </div>
+                            </div>
+                        </div>
+                    </Paper>
+                )
+            })
+        }
+        </Carousel>
+    </div>
   )
 }
 
